760ca5ee08a778e5c286ecaaf88f09abb090fd1e
Kamil Uyanik <uyanik@uni-bremen.de>
2023-06-13 19:42:25 +0200

    Vowel Counter Project

---
+++ Vowel Counter/script.js
@@ -0,0 +1,36 @@
+const buttonElement = document.querySelector('.js-submitButton')
+const inputElement = document.querySelector('.js-input')
+
+const vowels = 'aeiou'
+
+buttonElement.addEventListener('click', () => {
+  let count = 0
+  word = inputElement.value 
+
+  for (let i = 0; i < word.length; i++) {
+    for (let j = 0; j < vowels.length; j++) {
+      if (word[i] == vowels[j]) {
+        count++
+      }
+    }
+  }
+  inputElement.value = ''
+  alert(`Vowel Count: ${count}`)
+})
+
+inputElement.addEventListener('keydown', (event) => {
+  if (event.key == 'Enter') {
+    let count = 0
+    word = inputElement.value 
+
+    for (let i = 0; i < word.length; i++) {
+      for (let j = 0; j < vowels.length; j++) {
+        if (word[i] == vowels[j]) {
+          count++
+        }
+      } 
+    }
+    inputElement.value = ''
+    alert(`Vowel Count: ${count}`)
+    }
+}) 
