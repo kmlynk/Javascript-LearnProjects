77c3b09f9f0dc304c1ff6fa8d9440519ebcc9d66
Kamil Uyanik <uyanik@uni-bremen.de>
2023-06-21 01:16:28 +0200

    Random Quote Generator Project - v2

--- Random Quote Generator Project/script.js
+++ Random Quote Generator Project/script.js
@@ -1,15 +1,36 @@
 const generateBtn = document.querySelector('.generate-button')
 const quote = document.querySelector('.quote')
 const author = document.querySelector('.author .name')
+const soundBtn = document.querySelector('.sound')
+const copyBtn = document.querySelector('.copy')
+const twitterBtn = document.querySelector('.twitter')
 
 generateQuote()
 
 generateBtn.addEventListener('click', generateQuote)
 
-function generateQuote() {
-  fetch('https://api.quotable.io/random').then(res => res.json())
-    .then(result => {
-      quote.innerText = `"${result.content}"`
-      author.innerText = `- ${result.author}`
-  })
+  function generateQuote() {  
+    generateBtn.classList.add('loading')
+    generateBtn.innerText = 'Loading...'
+    fetch('https://api.quotable.io/random').then(res => res.json())
+      .then(result => {
+        quote.innerText = `${result.content}`
+        author.innerText = `${result.author}`
+        generateBtn.innerText = 'Generate'
+        generateBtn.classList.remove('loading')
+    })
 }
+
+// Speech Synthesis Utterance API - Speech Request
+soundBtn.addEventListener('click', () => {
+  let utterance = new SpeechSynthesisUtterance(`${quote.innerText} by ${author.innerText}`)
+  speechSynthesis.speak(utterance)
+})
+
+copyBtn.addEventListener('click', () => {
+  navigator.clipboard.writeText(`${quote.innerText} by ${author.innerText}`)
+})
+
+twitterBtn.addEventListener('click', () => {
+  window.open(`https://twitter.com/intent/tweet?text=${quote.innerText} \n - ${author.innerText}`)
+})
